9c1acb508192be01d7446a7d0335cd3cacab0591
Alex Beregszaszi <alex@rtfs.hu>
2022-06-27 09:53:25 +0200

    Add semantic test

---
+++ test/libsolidity/semanticTests/stdlib/sha256.sol
@@ -0,0 +1,12 @@
+import "std/precompiles.sol";
+
+contract C {
+  function f(uint256 a) external returns (bytes32) {
+    return sha256(abi.encodePacked(a));
+  }
+}
+
+// ====
+// stdlib: true
+// ----
+// f(uint256): 1 -> 0xec4916dd28fc4c10d78e287ca5d9cc51ee1ae73cbfde08c6b37324cbfaac8bc5
